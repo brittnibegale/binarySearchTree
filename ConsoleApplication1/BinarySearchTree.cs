ff98e5eac26c046d1d088f7d95ed30acc23ba8ef
brittnibegale <brittnibegale@gmail.com>
2017-10-03 09:35:18 -0500

    added files for binarySeachTree

---
+++ ConsoleApplication1/BinarySearchTree.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class BinarySearchTree
+    {
+        private Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        public Node Root
+        {
+            get
+            {
+                return root;
+            }
+            set
+            {
+                root = value;
+            }
+        }
+
+        public void Insert(int number)
+        {
+            Node newNode = new Node(number);
+            if (root == null)
+            {
+                this.root = newNode;
+            }
+            else
+            {
+                Node current = root;
+                while (true)
+                {
+                    Node tempParent = current;         
+                    if (tempParent.data < number)
+                    {
+                        current = current.rightChild;
+                        if(current == null)
+                        {
+                            tempParent.rightChild = newNode;
+                            return;
+                        }
+
+                    }
+                    else if (tempParent.data > number)
+                    {
+                        current = current.leftChild;
+                        if(current == null)
+                        {
+                            tempParent.leftChild = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Search(int number)
+        {
+            if(root == null)
+            {
+                Console.WriteLine("There is not a tree to search");
+            }
+            else
+            {
+                Node current = root;
+                while (true)
+                {
+                    Node tempParent = current;
+                    if (tempParent.data < number)
+                    {
+                        current = current.rightChild;
+                        if (current == null)
+                        {
+                            Console.WriteLine(number + " is not in the tree.");
+                            return;
+                        }
+                        else if (current.data == number)
+                        {
+                            Console.WriteLine("The tree contains " + number + ".");
+                            return;
+                        }
+                    }
+                    else if (tempParent.data > number)
+                    {
+                        current = current.leftChild;
+                        if (current == null)
+                        {
+                            Console.WriteLine(number + " is not in the tree.");
+                            return;
+                        }
+                        else if(current.data == number)
+                        {
+                            Console.WriteLine("The tree contains " + number + ".");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        
+        
+       
+    }
+}
